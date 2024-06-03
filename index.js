12ac4a31e0fca4ae2fa949e5d4c724093bc3b16b
joychandrauday <joychandraud@gmail.com>
2024-06-04 01:36:06 +0600

    set up the package details

--- index.js
+++ index.js
@@ -79,12 +79,12 @@ async function run() {
         res.status(500).send("Internal Server Error");
       }
     });
-    // app.get("/book/:id", async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = { _id: new ObjectId(id) };
-    //   const result = await bookCollection.findOne(query);
-    //   res.send(result);
-    // });
+    app.get("/package/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await packageCollection.findOne(query);
+      res.send(result);
+    });
 
 
     // app.post("/allbooks", async (req, res) => {
