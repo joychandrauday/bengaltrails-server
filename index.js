4ed1ef16aeae70717ec4d5f14c1e4b9ff734cab6
joychandrauday <joychandraud@gmail.com>
2024-06-03 10:57:14 +0600

    making sure all  are updatedd

--- index.js
+++ index.js
@@ -60,6 +60,12 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
+    app.get("/story/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await travelStoriesCollection.findOne(query);
+      res.send(result);
+    });
 
     // app.get("/book/:id", async (req, res) => {
     //   const id = req.params.id;
